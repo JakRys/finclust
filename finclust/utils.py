96053d5397fa1f77220a0d6eb13cd986ad3de696
JakRys <jakubrysavy00@gmail.com>
2022-11-17 07:49:01 +0100

    Create utils.py

---
+++ finclust/utils.py
@@ -0,0 +1,51 @@
+"""
+Utils
+
+This file provides an implementation of helping functions.
+"""
+
+from typing import Callable, Dict, List, Union
+
+import pandas as pd
+
+
+def calculate_similarities(data: pd.DataFrame, func: Callable, fillna_value: float = 0) -> pd.DataFrame:
+    if data.columns.nlevels > 1:
+        similarities = pd.DataFrame(columns=data.columns)
+        for col in data.columns.levels[0]:
+            similarities[col] = pd.DataFrame(func(data[col].T),
+                                             columns=data.columns.levels[1],
+                                             index=data.columns.levels[1]
+                                             ).fillna(fillna_value)
+        return similarities
+    return pd.DataFrame(func(data.T), columns=data.columns, index=data.columns).fillna(fillna_value)
+
+
+def compose_similarities(similarities: pd.DataFrame, weights: Union[List[float], Dict[str, float]] = None,
+                         normalize: bool = True) -> pd.DataFrame:
+    """
+    Calculate weighted sum of MultiIndex DataFrame.
+
+    Parameters
+    ----------
+    TODO:
+
+    Returns
+    -------
+    composed: pd.DataFrame
+        Non-MultiIndex DataFrame with weighted summed values.
+    """
+    if similarities.columns.nlevels == 1 or len(similarities.columns.levels[0]) == 1:
+        return similarities
+    columns = similarities.columns.levels[0]
+    if isinstance(weights, List):
+        weights = {c: v for c, v in zip(columns, weights)}
+    # composed = similarities.multiply(weights, axis="columns")
+    composed = similarities.copy()
+    if weights is not None:
+        for c, v in weights.items():
+            composed[c] *= v
+    composed = composed.groupby(level=1, axis="columns").sum()
+    if normalize and weights is not None:
+        return composed / sum(weights.values())
+    return composed
