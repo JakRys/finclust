b1a52feafac01d958b32d95126fbf42af9f77737
JakRys <jakubrysavy00@gmail.com>
2022-11-17 12:15:06 +0100

    Create evaluation.py

---
+++ finclust/evaluation.py
@@ -0,0 +1,69 @@
+"""
+Evaluation module
+
+"""
+
+import abc
+
+import pandas as pd
+import quantstats as qs
+
+
+class Evaluator:
+    """
+    Modul for evaluation of portfolio returns.
+
+    Parameters
+    ----------
+    TODO
+
+    Attributes
+    ----------
+    TODO
+    """
+
+    def __init__(self, plotting_backend: str = None) -> None:
+        self.plotting_backend = plotting_backend
+
+    @abc.abstractmethod
+    def evaluate(self, returns: pd.DataFrame) -> pd.DataFrame:
+        """
+        Runs the evaluation of returns.
+        """
+        raise NotImplementedError
+
+
+class QuantstatsEvaluator(Evaluator):
+
+    def __init__(self, benchmark=None, rf=0., display=False,
+            mode='basic', sep=False, compounded=True,
+            periods_per_year=252, prepare_returns=True,
+            match_dates=False, kwargs = {},
+            plotting_backend: str = None):
+        super().__init__(plotting_backend=plotting_backend)
+        self.benchmark = benchmark
+        self.rf = rf
+        self.display = display
+        self.mode = mode
+        self.sep = sep
+        self.compounded = compounded
+        self.periods_per_year = periods_per_year
+        self.prepare_returns = prepare_returns
+        self.match_dates = match_dates
+        self.kwargs = kwargs
+
+
+    def evaluate(self, returns: pd.DataFrame) -> pd.DataFrame:
+        return qs.reports.metrics(
+            returns,
+            benchmark = self.benchmark,
+            rf = self.rf,
+            display = self.display,
+            mode = self.mode,
+            sep = self.sep,
+            compounded = self.compounded,
+            periods_per_year = self.periods_per_year,
+            prepare_returns = self.prepare_returns,
+            match_dates = self.match_dates,
+            **self.kwargs
+        )
