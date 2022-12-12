9c9a0045fb4cfa46c5e0191b3d8fb74ed63bf5d8
JakRys <jakubrysavy00@gmail.com>
2022-12-12 22:50:49 +0100

    Create BuyAndHold clusterer

--- finclust/clustering.py
+++ finclust/clustering.py
@@ -5,7 +5,8 @@ Module for performing clustering.
 """
 
 import abc
-from typing import Callable, Dict
+from typing import Callable, Dict, Union
+
 import pandas as pd
 
 
@@ -53,9 +54,45 @@ class ScikitClusterer(Clusterer):
 
     def group(self, data: pd.DataFrame) -> Dict[str, int]:
         """
-        Calculate cluster based on provided similarities.
+        Calculate cluster based on provided affinities.
+
+        Parameters
+        ----------
+        data: pd.DataFrame
+            Pass the data to the group function
         """
         clustering = self.cluster_method.fit(data)
         symbols = clustering.feature_names_in_ if hasattr(clustering, "feature_names_in_") else data.columns
         labels = clustering.labels_
         return {symbol: label for symbol, label in zip(symbols, labels)}        
+
+
+class BuyAndHold(Clusterer):
+    """
+    A simple aggregator that buys everything at the beginning and keeps it until the end.
+
+    Parameters
+    ----------
+    label: Union[str, int], default=1
+        Label for portfolio marking
+    """
+
+    def __init__(self, name: str = "Buy&Hold", label: Union[str, int] = 1) -> None:
+        super().__init__(name)
+        self.label = label
+
+    def group(self, data: pd.DataFrame) -> Dict[str, int]:
+        """
+        A function that maps all data samples to a given label.
+
+        Parameters
+        ----------
+        data: pd.DataFrame
+            Pass the data to the group function
+        
+        Returns
+        -------
+            A dictionary where the keys are asset names and the values are integers
+        """
+        all_assets = data.columns.levels[1] if data.columns.nlevels > 1 else data.columns
+        return {asset: self.label for asset in all_assets}
